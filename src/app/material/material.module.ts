728936d23e23be40c9c11f93c96fad6429a17771
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2022-05-14 21:29:42 -0500

    feat: add styles

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 
 // Angular Material
+import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   exports: [
+    MatButtonModule,
     MatExpansionModule,
+    MatIconModule,
     MatListModule
   ]
 })
