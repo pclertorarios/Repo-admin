a9f05d62f27ef3992ac00292b55e8d6088b5928d
Piero Lertora <pclertora@outlook.com>
2022-06-03 14:36:04 -0500

    feat:actualizacion dialog update

--- src/app/dialogs/update/update.component.ts
+++ src/app/dialogs/update/update.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef,MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { ServicioService } from 'src/app/service/servicio.service';
+import { PostResponse,Category,Subcategory,Control} from 'src/app/interfaces/post.interface';
 @Component({
   selector: 'app-update',
   templateUrl: './update.component.html',
   styleUrls: ['./update.component.scss']
 })
+
+
 export class UpdateComponent implements OnInit {
 
-  constructor() { }
+  categoryupdate: Category={
+                    name:'',subcategories:[]};
+    myFormCategory: FormGroup = this.fb.group({
+      name:[,[Validators.required,Validators.minLength(1)]],
+      subcateogries:[,[Validators.required,Validators.min(3)]]
+    })
+  subcategoryupdate: Subcategory = {
+                    name: '', shortName: '',
+                    controls: []
+  };
+  myFormSubcategory: FormGroup =this.fb.group({
+    name:[,[Validators.required,Validators.min(3)]],
+    shortname:[,[Validators.required,Validators.minLength(1)]],
+    controls:[,[Validators.required,Validators.minLength(1)]]
+  })
+  controlupdate: Control = {name: ''}
+  myFormControl: FormGroup= this.fb.group({
+    name:[,[Validators.required,Validators.minLength(1)]]
+  });
+
+  constructor(private servicioService:ServicioService,
+              private fb:FormBuilder,
+              private dialogRef:MatDialogRef<UpdateComponent>, 
+              @Inject(MAT_DIALOG_DATA)public data: any) { }
 
   ngOnInit(): void {
+    const category: string = this.data.withCategory ? 'zzz': '';
+    this.myFormCategory.reset({
+      name:'',
+      subcategory: '',
+    })
+    const subcategory: string=this.data.withsuCategory ? 'zzz': '';
+    this.myFormSubcategory.reset({
+      name:'',
+      shortname: '',
+      controls:[]
+    })
+    this.myFormControl.reset({
+      name:'',
+    })
+    
   }
 
 }
+
+
---
+++ src/app/interfaces/update.data.interface.ts
@@ -0,0 +1,14 @@
+export interface updateCategory{
+    name: string,
+    subcategory: [],
+}
+
+export interface updateSubcategory{
+    name: string,
+    shortname:string,
+    control:[]
+}
+
+export interface updateControl{
+    name:string
+}
