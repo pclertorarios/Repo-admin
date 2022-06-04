452eb3a063d66454c2d589eda381178615146743
LuisMiguelEstradaFernandez <luismiguel.2205@hotmail.com>
2022-06-04 12:53:49 -0500

    feat: add navigation bmc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminUserComponent } from './pages/admin-user/admin-user.component';
+import { BmlControlComponent } from './pages/bml-control/bml-control.component';
 import { RepositorioComponent } from './pages/repositorio/repositorio.component';
 
 const routes: Routes = [
+  {
+    path: 'bmc',
+    component: BmlControlComponent
+  },
   {
     path: 'admin',
     component: AdminUserComponent
